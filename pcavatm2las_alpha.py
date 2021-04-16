a2a6e809dc562098267a7e85ebae76ea0f26da32
Charlie Xu <charliex@slac.stanford.edu>
2021-04-15 22:13:19 -0700

    FS11 atm

--- pcavatm2las_alpha.py
+++ pcavatm2las_alpha.py
@@ -1,5 +1,5 @@
-# pcav2las_alpha.py
-# script looks at the FEH CAST phase shifter value
+# pcavatm2las_alpha.py
+# script looks at the FEH CAST phase shifter value also the ATM value
 # and apply the mirror value to the FS11 drift correction PV 
 
 import epics as epics
@@ -9,8 +9,9 @@ import datetime
 
 # source /reg/g/pcds/engineering_tools/xpp/scripts/pcds_conda
 
-FS11_DC_sw_PV  = 'LAS:FS11:VIT:TT_DRIFT_ENABLE '  # Put 0 for disable, put 1 for enable
-FS11_DC_val_PV ='LAS:FS11:VIT:matlab:04'   # Drift correct value in ns
+FS11_DC_sw_PV  = 'LAS:FS11:VIT:TT_DRIFT_ENABLE'  # Put 0 for disable, put 1 for enable
+FS11_DC_val_PV = 'LAS:FS11:VIT:matlab:04'        # Drift correct value in ns
+FS11_ATM_PV  = 'XPP:TIMETOOL:TTALL'        # Waveform PV for reading ATM
 HXR_PCAV_PV0 = 'SIOC:UNDH:PT01:0:TIME0'     # EGU in ps
 HXR_PCAV_PV1 = 'SIOC:UNDH:PT01:0:TIME1'
 HXR_CAST_PS_PV_W = 'LAS:UND:MMS:02'         # EGU in ps
@@ -19,7 +20,7 @@ SXR_PCAV_PV0 = 'SIOC:UNDS:PT01:0:TIME0'
 SXR_PCAV_PV1 = 'SIOC:UNDS:PT01:0:TIME1'
 XPP_LAS_TT_PV = 'LAS:FS11:VIT:FS_TGT_TIME'  # EGU in ns
 
-
+# PCAV/CAST feed forward variables 
 cast_avg_n  = 20    # n sample moving average
 time_err_th = 20    # pcav err threshold in fs
 time_err_ary = np.zeros((cast_avg_n)) 
@@ -32,6 +33,21 @@ cntr = 0
 # PCAV0_setpoint = epics.caget(HXR_PCAV_PV0)
 # PCAV1_setpoint = epics.caget(HXR_PCAV_PV1)
 HXR_cast_iqs_sp = epics.caget(HXR_CAST_PS_PV_R)
+
+# ATM Feedback variables
+atm_avg_n = cast_avg_n
+atm_val_ary = np.zeros(atm_avg_n)
+ttamp_th = 1
+ipm2_th = 3000
+ttfwhm_hi = 130
+ttfwhm_lo = 70
+ATM_wf_val = epics.caget(FS11_ATM_PV)
+ATM_pos = ATM_wf_val[0]
+ATM_val = ATM_wf_val[1]
+ATM_amp = ATM_wf_val[2]
+ATM_nxt_amp = ATM_wf_val[3]
+ATM_ref_amp = ATM_wf_val[4]
+ATM_fwhm = ATM_wf_val[5]
 
 print('Controller running')
 while True:
@@ -52,6 +68,7 @@ while True:
     time_err_mean = np.mean(time_err_ary)
     time_err_mean_fs = np.around(np.multiply(time_err_mean, 1000), 3)
     pm_step = np.trunc(np.true_divide(time_err_mean_fs, time_err_th))
+
     if pm_step != motr_step_cntr:
         print('moving drift compensation')
         motr_step_diff = pm_step - motr_step_cntr
