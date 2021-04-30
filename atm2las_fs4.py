951c7daf34d7c58cc1163bdf9d78e200dde06395
Charlie Xu <charliex@slac.stanford.edu>
2021-04-29 23:40:49 -0700

    XCS prep

--- atm2las_fs4.py
+++ atm2las_fs4.py
@@ -39,5 +39,89 @@ ATM_nxt_amp = ATM_wf_val[3]
 ATM_ref_amp = ATM_wf_val[4]
 ATM_fwhm = ATM_wf_val[5]
 atm_pm_step = 0
+atm_prev = ATM_val
+atm_t_cntr = 1
+tt_good_cntr = 0
+tt_bad_cntr = 0
+cast_ff_en = 0
+cast_ff_cntr = 0
 
+# PCAV/CAST feed forward variables 
+DC_val = 0
+cast_avg_n  = 20    # n sample moving average
+cast_acc    = 0
+cast_acc_ns = 0
+cast_tot_acc  = 0
+time_err_th = 50    # pcav err threshold in fs
+time_err_ary = 0 
+time_err_prev = epics.caget(HXR_CAST_PS_PV_R)
+motr_step_cntr = 0  # counter for how many time the phase "motor" has moved in unit of time_err_th
+motr_step_diff_mag = 0 
+motr_step_diff_dir = 0 
+cntr = 0
+mv_cntr = 0
 
+print('Controller running')
+while True:
+    # Getting ATM & IPM reading determine if the ATM reading is good
+    atm_wf_tmp = epics.caget(ATM_PV)
+    atm_pos = atm_wf_tmp[0]
+    atm_val = atm_wf_tmp[1]
+    atm_amp = atm_wf_tmp[2]
+    atm_nxt_amp = atm_wf_tmp[3]
+    atm_ref_amp = atm_wf_tmp[4]
+    atm_fwhm = atm_wf_tmp[5]
+    ipm_val = epics.caget(IPM_PV)
+
+    # Condition for good atm reading
+    if (atm_amp > ttamp_th)and(atm_nxt_amp > ipm2_th)and(atm_fwhm < ttfwhm_hi)and(atm_fwhm > ttfwhm_lo)and(atm_val != atm_val_ary[-1,]):
+        tt_good_cntr += 1
+    else:
+        tt_bad_cntr += 1
+    
+    # Determine if use ATM or PCAV as drift compensation
+    if (tt_good_cntr > 250) and (atm_t_cntr%(3000*pause_time) == 0) :
+        print('good atm')
+        cast_ff_en = 0
+        tt_good_cntr = 0
+        tt_bad_cntr = 0
+        atm_t_cntr = 1
+    elif (tt_bad_cntr > 250) and (atm_t_cntr%(3000*pause_time) == 0) :
+        print('bad atm')
+        cast_ff_en = 1
+        cast_ff_cntr += 1
+        tt_good_cntr = 0
+        tt_bad_cntr = 0
+        atm_t_cntr = 1
+    elif (atm_t_cntr%(3000*pause_time) == 0) :
+        print('resetting')
+        atm_t_cntr = 1
+        tt_good_cntr = 0
+        tt_bad_cntr = 0
+    if (tt_good_cntr != 0) or (tt_bad_cntr != 0):
+        atm_t_cntr += 1    
+
+    # PCAV/CAST delta change of feedforward
+    cast_val_tmp = epics.caget(HXR_CAST_PS_PV_R)
+    if (cntr%(pause_time*50) == 0):
+        time_err_delta = cast_val_tmp - time_err_prev
+        time_err_d_fs = np.around(np.multiply(time_err_delta, 1000), 3)
+        cast_tot_acc = cast_tot_acc + time_err_d_fs
+        time_err_prev = cast_val_tmp
+        if cast_ff_en:
+            cast_acc = cast_acc + time_err_d_fs
+        else:
+            cast_acc = 0
+    if cast_acc>time_err_th or cast_acc<(-time_err_th):
+        print('Move to compensate')
+        cast_acc_ns = -np.true_divide(cast_acc, 1e6)
+        DC_val = epics.caget(DC_val_PV)
+        DC_val = DC_val + cast_acc_ns
+        # epics.caput(DC_val_PV, DC_val)
+        mv_cntr = mv_cntr + 1
+        cast_acc = 0
+
+    time_err_d_fs = np.around(np.multiply(time_err_delta, 1000), 3)
+
+    # Need to calculate ATM value and average
+    
