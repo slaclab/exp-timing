5d6a747d3ba5dd8019431e28d05578dd558be9dd
Charlie Xu <charliex@slac.stanford.edu>
2021-06-16 11:34:01 -0700

    update on pv list

--- atm2las.py
+++ atm2las.py
@@ -11,43 +11,52 @@ import datetime
 import yaml
 
 class PVS():
-    def __init__(self,hn):
-        pvlist = dict()
+    def __init__(self,hn):              
         filename = ("ATM_" + str(hn) + "_FB.yml")
         with open(filename, 'r') as yaml_file:
             self.yaml_content = yaml.load(yaml_file, Loader=yaml.SafeLoader)
-        print("Key: Value")
-        for key, value in self.yaml_content.items():
-            print(str(key)+": "+str(value))
 
-        self.pvlist["DC_sw_PV"]  = self.yaml_content.get("DC_sw_PV")
-        self.pvlist["DC_val_PV"] = self.yaml_content.get("DC_val_PV")
-        self.pvlist["ATM_FB_EN_PV"] = self.yaml_content.get("ATM_FB_EN_PV")
-        self.pvlist["ATM_FB_GAIN_PV"] = self.yaml_content.get("ATM_FB_GAIN_PV")
-        self.pvlist["ATM_WF_PV"] = self.yaml_content.get("ATM_WF_PV")
-        self.pvlist["ATM_TTC_PV"] = self.yaml_content.get("ATM_TTC_PV")
-        self.pvlist["IPM_PV"] = self.yaml_content.get("IPM_PV")
-        self.pvlist["HUTCH_XRAY_ST_PV"] = self.yaml_content.get["HUTCH_XRAY_ST_PV"]
-        self.pvlist["ATM_time_PV"] = self.yaml_content.get["ATM_time_PV"]
-        self.pvlist["ATM_amp_PV"] = self.yaml_content.get["ATM_amp_PV"]
-        self.pvlist["ATM_fwhm_PV"] = self.yaml_content.get["ATM_fwhm_PV"]
-        self.pvlist["PCAV_FB_EN_PV"] = self.yaml_content.get[""]
-        self.pvlist["PCAV_FB_GAIN_PV"] = self.yaml_content.get[""]
-        self.pvlist["PCAV_FB_OFFSET_PV"] = self.yaml_content.get[""]
-        self.pvlist["LXT_thre_PV"] = self.yaml_content.get["LXT_thre_PV"]
-        self.pvlist["LAS_TT_PV"] = self.yaml_content.get["LAS_TT_PV"]
-        self.pvlist["HXR_CAST_PS_PV_W"] = self.yaml_content.get["HXR_CAST_PS_PV_W"]
-        self.pvlist["SXR_CAST_PS_PV_W"] = self.yaml_content.get["SXR_CAST_PS_PV_W"]
-        self.atm_avg_n = self.yaml_content.get["atm_avg_n"]
-        self.time_err_th = self.yaml_content.get["time_err_th"]
-        self.pause_time = self.yaml_content.get["pause_time"]
+        # print("Key: Value")
+        # for key, value in self.yaml_content.items():
+        #     print(str(key)+": "+str(value))
+        
+        self.pvlist = dict()
+        
+        a = self.yaml_content.keys()
+        for pv in a:
+            # print(pv)
+            self.pvlist[pv] = self.yaml_content.get(pv)
+
+        # self.pvlist["DC_sw_PV"]  = self.yaml_content.get("DC_sw_PV")
+        # self.pvlist["DC_val_PV"] = self.yaml_content.get("DC_val_PV")
+        # self.pvlist["ATM_FB_EN_PV"] = self.yaml_content.get("ATM_FB_EN_PV")
+        # self.pvlist["ATM_FB_GAIN_PV"] = self.yaml_content.get("ATM_FB_GAIN_PV")
+        # self.pvlist["ATM_WF_PV"] = self.yaml_content.get("ATM_WF_PV")
+        # self.pvlist["ATM_TTC_PV"] = self.yaml_content.get("ATM_TTC_PV")
+        # self.pvlist["IPM_PV"] = self.yaml_content.get("IPM_PV")
+        # self.pvlist["HUTCH_XRAY_ST_PV"] = self.yaml_content.get("HUTCH_XRAY_ST_PV")
+        # self.pvlist["ATM_time_PV"] = self.yaml_content.get("ATM_time_PV")
+        # self.pvlist["ATM_amp_PV"] = self.yaml_content.get("ATM_amp_PV")
+        # self.pvlist["ATM_fwhm_PV"] = self.yaml_content.get("ATM_fwhm_PV")
+        # self.pvlist["PCAV_FB_EN_PV"] = self.yaml_content.get("PCAV_FB_EN_PV")
+        # self.pvlist["PCAV_FB_GAIN_PV"] = self.yaml_content.get("PCAV_FB_GAIN_PV")
+        # self.pvlist["PCAV_FB_OFFSET_PV"] = self.yaml_content.get("PCAV_FB_OFFSET_PV")
+        # self.pvlist["LXT_thre_PV"] = self.yaml_content.get("LXT_thre_PV")
+        # self.pvlist["LAS_TT_PV"] = self.yaml_content.get("LAS_TT_PV")
+        # self.pvlist["HXR_CAST_PS_PV_W"] = self.yaml_content.get("HXR_CAST_PS_PV_W")
+        # self.pvlist["SXR_CAST_PS_PV_W"] = self.yaml_content.get("SXR_CAST_PS_PV_W")
+        self.atm_avg_n = self.yaml_content.get("atm_avg_n")
+        self.time_err_th = self.yaml_content.get("time_err_th")
+        self.pause_time = self.yaml_content.get("pause_time")
 
 def drift_comp_fb(hutch='NULL'):
     P = PVS(hutch)
-    print(P.yaml_content.get("IPM_PV"))
-    print(P.DC_sw_PV)
-    DC_val = epics.caget(P.DC_val_PV)
-    print(str(DC_val))
+    print(P.pvlist.items())
+    # print(P.yaml_content.get("IPM_PV"))
+    # print(P.DC_sw_PV)
+    # DC_val = epics.caget(P.DC_val_PV)
+    # print(str(DC_val))
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
